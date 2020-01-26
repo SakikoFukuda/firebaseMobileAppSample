47140132a672e61326538e1d474bb4a656e69b5c
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-01-26 22:52:13 +0900

    ボタン押下時のイベントと、それぞれのボタンにタグを設定。このタグで「どのマスが押されたか」を判断する

--- mobile/app/src/main/java/com/example/myapplication/MainActivity.java
+++ mobile/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println(v.getTag());
     }
 }
