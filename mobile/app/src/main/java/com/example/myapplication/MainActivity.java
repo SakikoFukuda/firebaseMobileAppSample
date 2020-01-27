00a8cff8eceee31bcb7c56cfbeea40560c7bd737
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-01-27 22:10:45 +0900

    とりあえずゲームができるように

--- mobile/app/src/main/java/com/example/myapplication/MainActivity.java
+++ mobile/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,17 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.myapplication.marubatu.MaruBatuBoard;
+import com.example.myapplication.marubatu.Turn;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TURN_FIRST  = "○の番です";
+    private final String TURN_SECOND = "×の番です";
+
+    private Turn currentTurn;
+    private final MaruBatuBoard board = new MaruBatuBoard();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.turn_comment);
+        textView.setText(TURN_FIRST);
+        currentTurn = Turn.FIRST;
     }
 
     @Override
     public void onClick(View v) {
-        System.out.println(v.getTag());
+        Button button = (Button) v;
+
+        switch (currentTurn){
+            case FIRST:
+                button.setText("○");
+                board.add(button, MaruBatuBoard.TOKEN.MARU);
+                judge(button, MaruBatuBoard.TOKEN.MARU);
+                break;
+            case SECOND:
+                button.setText("×");
+                board.add(button, MaruBatuBoard.TOKEN.BATU);
+                judge(button, MaruBatuBoard.TOKEN.BATU);
+                break;
+        }
+    }
+
+    private void judge(Button button, MaruBatuBoard.TOKEN token) {
+        if (board.judge(button, token)) {
+            TextView textView = (TextView) findViewById(R.id.maru_batu_result);
+            textView.setText("勝負あり！！");
+        } else {
+            toggleTurn();
+        }
+    }
+
+    private void toggleTurn() {
+        TextView textView = (TextView) findViewById(R.id.turn_comment);
+
+        if (currentTurn.equals(Turn.FIRST)) {
+            textView.setText(TURN_SECOND);
+            currentTurn = Turn.SECOND;
+        } else if (currentTurn.equals(Turn.SECOND)) {
+            textView.setText(TURN_FIRST);
+            currentTurn = Turn.FIRST;
+        }
     }
 }
---
+++ mobile/app/src/main/java/com/example/myapplication/marubatu/MaruBatuBoard.java
@@ -0,0 +1,130 @@
+package com.example.myapplication.marubatu;
+
+import android.widget.Button;
+
+public class MaruBatuBoard {
+
+    public enum BOARD_SQUARE {
+        TOP_LEFT("TopLeft"),
+        TOP_CENTER("TopCenter"),
+        TOP_RIGHT("TopRight"),
+        MIDDLE_LEFT("MiddleLeft"),
+        MIDDLE_CENTER("MiddleCenter"),
+        MIDDLE_RIGHT("MiddleRight"),
+        BOTTOM_LEFT("BottomLeft"),
+        BOTTOM_CENTER("BottomCenter"),
+        BOTTOM_RIGHT("BottomRight");
+
+        String value;
+
+        BOARD_SQUARE(String s) {
+            this.value = s;
+        }
+
+        public static BOARD_SQUARE of(String s) {
+            for (BOARD_SQUARE bs : BOARD_SQUARE.values()) {
+                if (bs.value.equals(s)) {
+                    return bs;
+                }
+            }
+            throw new IllegalArgumentException(s);
+        }
+
+        public boolean equals(String s) {
+            return this.value.equals(s);
+        }
+    }
+
+    public enum TOKEN {
+        MARU,
+        BATU,
+        NONE
+    }
+
+    private TOKEN[][] board = { {TOKEN.NONE, TOKEN.NONE, TOKEN.NONE}, {TOKEN.NONE, TOKEN.NONE, TOKEN.NONE}, {TOKEN.NONE, TOKEN.NONE, TOKEN.NONE} };
+
+    public void add(Button button, TOKEN token) {
+        BOARD_SQUARE square = BOARD_SQUARE.of((String) button.getTag());
+
+        switch (square) {
+            case TOP_LEFT:
+                board[0][0] = token;
+                break;
+            case TOP_CENTER:
+                board[0][1] = token;
+                break;
+            case TOP_RIGHT:
+                board[0][2] = token;
+                break;
+            case MIDDLE_LEFT:
+                board[1][0] = token;
+                break;
+            case MIDDLE_CENTER:
+                board[1][1] = token;
+                break;
+            case MIDDLE_RIGHT:
+                board[1][2] = token;
+                break;
+            case BOTTOM_LEFT:
+                board[2][0] = token;
+                break;
+            case BOTTOM_CENTER:
+                board[2][1] = token;
+                break;
+            case BOTTOM_RIGHT:
+                board[2][2] = token;
+                break;
+        }
+    }
+
+    public boolean judge(Button button, TOKEN token) {
+        BOARD_SQUARE square = BOARD_SQUARE.of((String) button.getTag());
+
+        switch (square) {
+            case TOP_LEFT:
+                return (board[0][1].equals(token) && board[0][2].equals(token)) // 横
+                        || (board[1][0].equals(token) && board[2][0].equals(token)) // 縦
+                        || (board[1][1].equals(token) && board[2][2].equals(token)) // 斜め
+                        ;
+            case TOP_CENTER:
+                return (board[0][0].equals(token) && board[0][2].equals(token)) // 横
+                        || (board[1][1].equals(token) && board[2][1].equals(token)) // 縦
+                        ;
+            case TOP_RIGHT:
+                return (board[0][0].equals(token) && board[0][1].equals(token)) // 横
+                        || (board[1][2].equals(token) && board[2][2].equals(token)) // 縦
+                        || (board[1][1].equals(token) && board[2][0].equals(token)) // 斜め
+                        ;
+            case MIDDLE_LEFT:
+                return (board[1][1].equals(token) && board[1][2].equals(token)) // 横
+                        || (board[0][0].equals(token) && board[2][0].equals(token)) // 縦
+                        ;
+            case MIDDLE_CENTER:
+                return (board[1][0].equals(token) && board[1][2].equals(token)) // 横
+                        || (board[0][1].equals(token) && board[2][1].equals(token)) // 縦
+                        || (board[0][0].equals(token) && board[2][2].equals(token)) // 斜め
+                        || (board[0][2].equals(token) && board[2][0].equals(token)) // 斜め
+                        ;
+            case MIDDLE_RIGHT:
+                return (board[1][0].equals(token) && board[1][2].equals(token)) // 横
+                        || (board[0][2].equals(token) && board[2][2].equals(token)) // 縦
+                        ;
+            case BOTTOM_LEFT:
+                return (board[2][1].equals(token) && board[2][0].equals(token)) // 横
+                        || (board[0][0].equals(token) && board[1][0].equals(token)) // 縦
+                        || (board[0][2].equals(token) && board[1][1].equals(token)) // 斜め
+                        ;
+            case BOTTOM_CENTER:
+                return (board[2][0].equals(token) && board[2][2].equals(token)) // 横
+                        || (board[0][1].equals(token) && board[1][1].equals(token)) // 縦
+                        ;
+            case BOTTOM_RIGHT:
+                return (board[2][0].equals(token) && board[2][1].equals(token)) // 横
+                        || (board[0][2].equals(token) && board[1][2].equals(token)) // 縦
+                        || (board[0][0].equals(token) && board[1][1].equals(token)) // 斜め
+                        ;
+        }
+
+        return false;
+    }
+}
